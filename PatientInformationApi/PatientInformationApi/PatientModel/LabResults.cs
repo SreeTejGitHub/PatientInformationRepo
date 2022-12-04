ee2f82c33fc78a18bb21597e21565c082c08c4de
Sree Tej Peddaiahgari <sree7795@gmail.com>
2022-12-04 13:16:00 -0600

    Added patient and lab result models

---
+++ PatientInformationApi/PatientInformationApi/PatientModel/LabResults.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace PatientInformationApi.PatientModel
+{
+	public class LabResults
+	{
+        public int labId { get; set; }
+        public string name { get; set; }
+        public string value { get; set; }
+        public string valueType { get; set; }
+        public string timeOfTest { get; set; }
+        public string enteredTime { get; set; }
+    }
+}
+
---
+++ PatientInformationApi/PatientInformationApi/PatientModel/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace PatientInformationApi.PatientModel
+{
+	public class Patient
+	{
+	
+		public int patientId { get; set; }
+		public string firstName { get; set; }
+        public string middleName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public string phoneNumber { get; set; }
+        public string emailAddress { get; set; }
+        public string dateOfAdmission { get; set; }
+    }
+}
+
